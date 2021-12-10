9d1e8fcaf372019ccfb8c7c308ee1b19aef6a84c
Skizaat <77202823+Skizaat@users.noreply.github.com>
2021-12-11 00:16:05 +0100

    fragments model added for people

--- app/src/main/java/com/example/myghibli/TestActivity.java
+++ app/src/main/java/com/example/myghibli/TestActivity.java
@@ -1,8 +1,13 @@
 package com.example.myghibli;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        Button peopleBtn = (Button) findViewById(R.id.peoplebutton);
+        peopleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment informFragment = fm.findFragmentById(R.id.informFragment);
+                fm.beginTransaction()
+                        .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                        .show(informFragment)
+                        .commit();
+            }
+        });
+
+        Fragment informFragment = fm.findFragmentById(R.id.informFragment);
+        fm.beginTransaction()
+                .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                .hide(informFragment)
+                .commit();
     }
+
+
 }
